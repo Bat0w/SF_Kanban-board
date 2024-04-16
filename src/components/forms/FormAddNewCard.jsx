03854774787fe70fe0d653513bca2d8e7d450e21
Name <email@example.ru>
2024-04-16 14:11:43 +0300

    version with corrections based on mentors comments

---
+++ src/components/forms/FormAddNewCard.jsx
@@ -0,0 +1,46 @@
+import React from "react";
+import { useState } from "react";
+import css from './Form.module.css'
+
+const FormAddNewCard = props => {
+    const { formSubmit } = props;
+    const [values, setValues] = useState({
+        title: ''
+    })
+
+    const handleChange = e => {
+        const fieldName = e.target.name
+        setValues({...values, [fieldName]: e.target.value})
+    }
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        if (values.title) {
+          formSubmit(values.title, values.description);
+        }
+    };
+    
+    return (
+        <form className={css.form} onSubmit={handleSubmit}>
+            <input 
+            className={css.input}
+            id='taskTitle'
+            name='title'
+            type='text'
+            placeholder='Enter New task title...'
+            value={values.title}
+            onChange={handleChange} 
+            />
+            <button 
+                className={css.submit} 
+                type="submit"
+                disabled={!values.title}
+                style={!values.title ? { cursor: "default", background: "#0079BF" } : null}
+                >
+                Submit
+            </button>
+        </form>
+    )
+}
+
+export default FormAddNewCard
